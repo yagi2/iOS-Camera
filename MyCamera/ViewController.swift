e791325315f0cadcb76675d5d18a66a7e1de1e7a
Itsuki Aoyagi <mail@yagi2.dev>
2021-06-02 22:18:12 +0900

    create layout

--- MyCamera/ViewController.swift
+++ MyCamera/ViewController.swift
@@ -8,12 +8,15 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var pictureImage: UIImageView!
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBAction func cameraButtonAction(_ sender: Any) {
+        
     }
-
-
+    
+    @IBAction func shareButtonAction(_ sender: Any) {
+        
+    }
 }
 
