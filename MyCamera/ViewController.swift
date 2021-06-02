234dfb04eba76aacb50f90862d52c483dd3fae54
Itsuki Aoyagi <mail@yagi2.dev>
2021-06-02 22:04:18 +0900

    initial commit

---
+++ MyCamera/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyCamera
+//
+//  Created by Itsuki Aoyagi on 2021/06/02.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
