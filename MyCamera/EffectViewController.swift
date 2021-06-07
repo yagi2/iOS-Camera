6a8c162942d7dace3c4789a61aae18bc01841489
Itsuki Aoyagi <mail@yagi2.dev>
2021-06-07 22:21:15 +0900

    add some views to effect view controller screen

--- MyCamera/EffectViewController.swift
+++ MyCamera/EffectViewController.swift
@@ -8,22 +8,17 @@
 import UIKit
 
 class EffectViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    @IBOutlet weak var effectImage: UIImageView!
+    
+    @IBAction func effectButtonAction(_ sender: Any) {
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func shareButtonAction(_ sender: Any) {
+        
     }
-    */
-
+    
+    @IBAction func closeButtonAction(_ sender: Any) {
+        
+    }
 }
